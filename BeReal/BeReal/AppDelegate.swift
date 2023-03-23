2ccd2ab0b47ff63b76a06c18714705994c08f358
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-03-23 17:38:44 -0400

    icon

--- BeReal/BeReal/AppDelegate.swift
+++ BeReal/BeReal/AppDelegate.swift
@@ -19,21 +19,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                               clientKey: "50bavJUvCxtZAg10BT6Lv9j7MRTZ2XGJZDYTGDM6",
                               serverURL: URL(string: "https://parseapi.back4app.com")!)
         
-        // Instantiate the test parse object
-        var score = GameScore()
-        score.playerName = "Kingsley"
-        score.points = 13
-
-        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
-        // If no callbackQueue is specified it returns to main queue.
-        score.save { result in
-            switch result {
-            case .success(let savedScore):
-                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
-            case .failure(let error):
-                assertionFailure("Error saving: \(error)")
-            }
-        }
+//        // Instantiate the test parse object
+//        var score = GameScore()
+//        score.playerName = "Kingsley"
+//        score.points = 13
+//
+//        // Save to your server asynchronously (preferred way) - Performs work on background queue and returns to specified callbackQueue.
+//        // If no callbackQueue is specified it returns to main queue.
+//        score.save { result in
+//            switch result {
+//            case .success(let savedScore):
+//                print("✅ Parse Object SAVED!: Player: \(String(describing: savedScore.playerName)), Score: \(String(describing: savedScore.points))")
+//            case .failure(let error):
+//                assertionFailure("Error saving: \(error)")
+//            }
+//        }
         
         return true
     }
@@ -54,36 +54,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
-// Create your own value type `ParseObject`.
-struct GameScore: ParseObject {
-    // These are required by ParseObject
-    var objectId: String?
-    var createdAt: Date?
-    var updatedAt: Date?
-    var ACL: ParseACL?
-    var originalData: Data?
-
-    // Your own custom properties.
-    // All custom properties must be optional.
-    var playerName: String?
-    var points: Int?
-}
-
-// Sample Usage
+//// Create your own value type `ParseObject`.
+//struct GameScore: ParseObject {
+//    // These are required by ParseObject
+//    var objectId: String?
+//    var createdAt: Date?
+//    var updatedAt: Date?
+//    var ACL: ParseACL?
+//    var originalData: Data?
 //
-// var score = GameScore()
-// score.playerName = "Kingsley"
-// score.points = 13
-
-// OR Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
-// It's recommended to place custom initializers in an extension
-// to preserve the memberwise initializer.
-extension GameScore {
-
-    // Use the init to set your custom properties
-    // NOTE: Properties in custom init are NOT required to be optional
-    init(playerName: String, points: Int) {
-        self.playerName = playerName
-        self.points = points
-    }
-}
+//    // Your own custom properties.
+//    // All custom properties must be optional.
+//    var playerName: String?
+//    var points: Int?
+//}
+//
+//// Sample Usage
+////
+//// var score = GameScore()
+//// score.playerName = "Kingsley"
+//// score.points = 13
+//
+//// OR Implement a custom initializer (OPTIONAL i.e. NOT REQUIRED)
+//// It's recommended to place custom initializers in an extension
+//// to preserve the memberwise initializer.
+//extension GameScore {
+//
+//    // Use the init to set your custom properties
+//    // NOTE: Properties in custom init are NOT required to be optional
+//    init(playerName: String, points: Int) {
+//        self.playerName = playerName
+//        self.points = points
+//    }
+//}
