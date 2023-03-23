6acbd930f1361ccd1d96fe1c1128f9e502db80a8
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-03-23 10:11:30 -0400

    new project

---
+++ BeReal/BeReal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeReal
+//
+//  Created by 靳肖阳 on 3/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
