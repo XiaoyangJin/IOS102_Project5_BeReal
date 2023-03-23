749f676eb210e33a8dad2f6a4dac34daa9a45264
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-03-23 17:37:14 -0400

    test successful for parse use

--- BeReal/BeReal/Login/LoginViewController.swift
+++ BeReal/BeReal/Login/LoginViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import ParseSwift
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
